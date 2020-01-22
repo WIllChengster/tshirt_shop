f3bdb3ea15c02328b077939758c91ae194a07427
burninggun <willchengsters@gmail.com>
2020-01-22 15:52:17 -0800

    init react

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar" >
+            <h3 className="navbar-brand">T-Shirt Shop</h3>
+        </div>
+    )
+}
+
+export default Navbar
