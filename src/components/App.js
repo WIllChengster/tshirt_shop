1b6c67fbb68a61795f2c91fab9717466c5af1a10
burninggun <willchengsters@gmail.com>
2020-01-23 10:35:22 -0800

    starting featured component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar'
+import Featured from './Featured';
 import './App.css';
+
 function App() {
 	return (
 		<div>
 			<Navbar/>
+			<Featured/>
 		</div>
 	);
 }
 
 export default App;
+ 
---
+++ src/components/Featured.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Featured.css'
+
+const Featured = () => {
+    return(
+        <div className="featured-container" >
+            <h3>Featured Projects</h3>
+            <div className="products-container">
+                <div className="products" >
+                    <div className="product-pic">
+                        
+                    </div>
+                    <p>product name</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
