ec0c449166dfb64cc834f1e4b0645c9043dff805
burninggun <willchengsters@gmail.com>
2020-02-08 19:07:39 -0800

    got node to serve static files from different difrs

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000
+
+
+app.use(
+        express.static(
+            path.resolve( 
+                    __dirname, 'client', 'build'
+                )
+        )
+    );
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build'), 'index.html')
+    console.log(
+        path.resolve(__dirname, "client", "public", "index.html")
+    )
+
+
+})
 
 app.listen(PORT, () => {
     console.log('Node server is running on PORT:', PORT)
